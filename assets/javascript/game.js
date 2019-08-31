aa3819bfadb826f6e0eb5a59213409b5cbca059e
Damien Aponte <damienapo@gmail.com>
2019-08-30 20:32:57 -0400

    added global variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var targetNum = Math.floor(Math.random() * 101) + 19;
+var crystal1 = $('#crystal-1');
+var crystal2 = $('#crystal-2');
+var crystal3 = $('#crystal-3');
+var crystal4 = $('#crystal-4');
+var crystalsAll = $('.crystal');
+var crystalVal1 = 0;
+console.log(targetNum);
+
