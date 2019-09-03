56eefc1bd94da4b383f4c2eabe1147e4f86ad45e
Damien Aponte <damienapo@gmail.com>
2019-09-03 11:08:32 -0400

    added win/lose condition

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,43 +9,77 @@ var crystalVal1 = Math.floor(Math.random() * 12) + 1;
 var crystalVal2 = Math.floor(Math.random() * 12) + 1;
 var crystalVal3 = Math.floor(Math.random() * 12) + 1;
 var crystalVal4 = Math.floor(Math.random() * 12) + 1;
-console.log(targetNum);
 console.log(crystalVal1);
 console.log(crystalVal2);
 console.log(crystalVal3);
 console.log(crystalVal4);
 
-// $('#crystals').on('click', function () {
-//     $('#crystal-1')[0].innerHTML = parseInt($('#crystal-1')[0].innerHTML) + 1;
-// });
 
 crystal1.on('click', function() {
     userScore += crystalVal1;
     $('#score').text(userScore);
-})
+    if (userScore === targetNum) {
+        console.log('win')
+        $('#crystals').text('WINNER!!!')
+    };
+
+    if (userScore > targetNum) {
+        console.log('lose')
+        $('#crystals').text('YOU LOSE!!!')
+     };
+});
 
 crystal2.on('click', function() {
     userScore += crystalVal2;
     $('#score').text(userScore);
-    console.log(userScore);
-})
+    if (userScore === targetNum) {
+        console.log('win')
+        $('#crystals').text('WINNER!!!')
+    };
+
+    if (userScore > targetNum) {
+        console.log('lose')
+        $('#crystals').text('YOU LOSE!!!')
+     };
+});
 
 crystal3.on('click', function() {
     userScore += crystalVal3;
     $('#score').text(userScore);
-    console.log(userScore);
-})
+    if (userScore === targetNum) {
+        console.log('win')
+        $('#crystals').text('WINNER!!!')
+    };
+
+    if (userScore > targetNum) {
+        console.log('lose')
+        $('#crystals').text('YOU LOSE!!!')
+     };
+});
 
 crystal4.on('click', function() {
     userScore += crystalVal4;
     $('#score').text(userScore);
-    console.log(userScore);
-})
+    if (userScore === targetNum) {
+        console.log('win')
+        $('#crystals').text('WINNER!!!')
+    };
 
-// document.getElementById("crystals").addEventListener("click", function(){
-//     $(this).alert('YOOOOOO');
-//   });
-console.log(userScore);
+    if (userScore > targetNum) {
+        console.log('lose')
+        $('#crystals').text('YOU LOSE!!!')
+     };
+});
+
+ if (userScore === targetNum) {
+     console.log('win')
+     $('#crystals').text('WINNER!!!')
+ };
+
+ if (userScore > targetNum) {
+    console.log('lose')
+    $('#crystals').text('YOU LOSE!!!')
+ };
 
 $('#target').text(targetNum);
 $('#score').text(userScore);
