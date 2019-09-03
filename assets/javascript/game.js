20f7abf4bcbc75ca7c2a8042d63b4988c341fda8
Damien Aponte <damienapo@gmail.com>
2019-09-03 13:36:07 -0400

    game rules work small changes to css. added win loss counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
 var targetNum = Math.floor(Math.random() * 102) + 19;
 var userScore = 0;
+var wins = 0;
+var losses = 0;
 var crystal1 = $('#crystal-1');
 var crystal2 = $('#crystal-2');
 var crystal3 = $('#crystal-3');
@@ -13,73 +15,80 @@ console.log(crystalVal1);
 console.log(crystalVal2);
 console.log(crystalVal3);
 console.log(crystalVal4);
+function reset() {
+    userScore = 0;
+    targetNum = Math.floor(Math.random() * 102) + 19;
+    $('#score').text(userScore);
+    $('#target').text(targetNum);
+}
 
-
-crystal1.on('click', function() {
+crystal1.on('click', function () {
     userScore += crystalVal1;
-    $('#score').text(userScore);
-    if (userScore === targetNum) {
-        console.log('win')
-        $('#crystals').text('WINNER!!!')
-    };
-
-    if (userScore > targetNum) {
-        console.log('lose')
-        $('#crystals').text('YOU LOSE!!!')
-     };
 });
 
-crystal2.on('click', function() {
+crystal2.on('click', function () {
     userScore += crystalVal2;
-    $('#score').text(userScore);
-    if (userScore === targetNum) {
-        console.log('win')
-        $('#crystals').text('WINNER!!!')
-    };
-
-    if (userScore > targetNum) {
-        console.log('lose')
-        $('#crystals').text('YOU LOSE!!!')
-     };
 });
 
-crystal3.on('click', function() {
+crystal3.on('click', function () {
     userScore += crystalVal3;
-    $('#score').text(userScore);
-    if (userScore === targetNum) {
-        console.log('win')
-        $('#crystals').text('WINNER!!!')
-    };
-
-    if (userScore > targetNum) {
-        console.log('lose')
-        $('#crystals').text('YOU LOSE!!!')
-     };
 });
 
-crystal4.on('click', function() {
+crystal4.on('click', function () {
     userScore += crystalVal4;
+});
+
+crystalsAll.on('click', function () {
     $('#score').text(userScore);
     if (userScore === targetNum) {
-        console.log('win')
-        $('#crystals').text('WINNER!!!')
+        console.log('win');
+        $('#result').text('WINNER!!!');
+        $('#result').append('<button id="reset"> Try Again? </button>');
+        $('#reset').on('click', function () {
+            userScore = 0;
+            targetNum = Math.floor(Math.random() * 102) + 19;
+            crystalVal1 = Math.floor(Math.random() * 12) + 1;
+            crystalVal2 = Math.floor(Math.random() * 12) + 1;
+            crystalVal3 = Math.floor(Math.random() * 12) + 1;
+            crystalVal4 = Math.floor(Math.random() * 12) + 1;
+            wins =+ 1;
+            $('wins').text(wins)
+            $('#score').text(userScore);
+            $('#target').text(targetNum);
+            $('#result').empty();
+            console.log(crystalVal1);
+            console.log(crystalVal2);
+            console.log(crystalVal3);
+            console.log(crystalVal4);
+            
+
+        });
     };
 
     if (userScore > targetNum) {
         console.log('lose')
-        $('#crystals').text('YOU LOSE!!!')
-     };
+        $('#result').text('YOU LOSE!!!');
+        $('#result').append('<button id="reset"> Try Again? </button>')
+        $('#reset').on('click', function () {
+            userScore = 0;
+            targetNum = Math.floor(Math.random() * 102) + 19;
+            crystalVal1 = Math.floor(Math.random() * 12) + 1;
+            crystalVal2 = Math.floor(Math.random() * 12) + 1;
+            crystalVal3 = Math.floor(Math.random() * 12) + 1;
+            crystalVal4 = Math.floor(Math.random() * 12) + 1;
+            losses =+ 1;
+            $('losses').text(losses)
+            $('#score').text(userScore);
+            $('#target').text(targetNum);
+            $('#result').empty();
+            console.log(crystalVal1);
+            console.log(crystalVal2);
+            console.log(crystalVal3);
+            console.log(crystalVal4);
+        });
+    };
 });
-
- if (userScore === targetNum) {
-     console.log('win')
-     $('#crystals').text('WINNER!!!')
- };
-
- if (userScore > targetNum) {
-    console.log('lose')
-    $('#crystals').text('YOU LOSE!!!')
- };
-
 $('#target').text(targetNum);
 $('#score').text(userScore);
+
+
